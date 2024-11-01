45e4f25937b99e62a44ecd3bc9d5296cec6988f1
ranjansharma-ui <ranjankumar4278907@gmail.com>
2024-11-01 06:03:45 +0000

    DATA TYPES

---
+++ javascript/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3); // we are using nodejs, not browser
+
+console.log(3 + 3); 
+console.log("Ranjan")
+
+let name = "RAnjan" // hitesh
+let age = 18 // integer
+let isLoggedIn = false //boolean
+
+/*
+  number => 2 to power 53
+  bigint
+  string => ""
+  boolean => true/false
+  null => standalone value
+  undefind => not asign any value
+  symbol => unique
+
+  //object
+
+*/
+console.log(typeof(null)) // object
+console.log(typeof(undefined)); //indefined
+
