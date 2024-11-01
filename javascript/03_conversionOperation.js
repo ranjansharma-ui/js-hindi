0a371c2b7ffb1bffba8b4dde09354a48d682b95a
ranjansharma-ui <ranjankumar4278907@gmail.com>
2024-11-01 06:30:56 +0000

    CONVERSION OPERATION

---
+++ javascript/03_conversionOperation.js
@@ -0,0 +1,41 @@
+let score = "33abd"
+/*
+-> use
+score = null
+score = undefined
+score = "RAnjan"
+score = true
+*/
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//note
+//"33" => 33
+//"33abs" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 0;
+/*
+isLoggedIn = ""
+isLoggedIn = "Ranjan"
+*/
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
